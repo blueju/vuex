ae993e705e6d430906c4f0e50e0b808a0b02603f
blueju <blueju@foxmail.com>
2019-08-09 20:23:21 +0800

    初始化

--- src/components/vue-study.vue
+++ src/components/vue-study.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
-    <!-- <section>{{ this.$store.state.good.good_test_data }}</section> -->
-    <!-- <section>{{ this.$store.getters.get_good_test_data }}</section> -->
-    <div class="increment">
-      <button @click="increment">点击</button>
-      <h1>{{ this.$store.state.number }}</h1>
-    </div>
+    <!-- 自增区 -->
+    <section class="increment">
+      <button @click="increment">加一</button>
+      <h1>{{ this.$store.state.increment.number }}</h1>
+    </section>
+
+    <!-- 自减区 -->
+    <section class="decrement">
+      <button @click="decrement">减一</button>
+      <h1>{{ this.$store.state.decrement.number }}</h1>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
   // 给组件命名（可选）
-  name: "HelloWorld",
+  name: "VueStudy",
   // 数据
   data() {
     return {};
@@ -22,7 +27,14 @@ export default {
     increment() {
       // 点击按钮后，我们需要自增加一，我们可以将这个自增加一理解成一个任务，现使用dispatch方法将任务派发给actions_increment
       this.$store.dispatch("actions_increment");
+    },
+    decrement() {
+      // 点击按钮后，我们需要自减减一，我们可以将这个自减减一理解成一个任务，现使用dispatch方法将任务派发给actions_increment
+      this.$store.dispatch("actions_decrement");
     }
+  },
+  mounted() {
+    console.log(this.$store);
   }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,30 +3,13 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+import increment from "./modules/increment"
+import decrement from "./modules/decrement"
+
 const store = new Vuex.Store({
-    // 数据存储仓库
-    state: {
-        number: 0
-    },
-    // 改变 state 数据的方法，
-    mutations: {
-        /**
-         * 自增方法
-         * @param {*} state 需要传入参数state，即数据存储仓库
-         */
-        mutations_increment(state) {
-            state.number = state.number + 1
-        }
-    },
-    // 触发（改变 state 数据的方法）的方法
-    actions: {
-        /**
-         * 触发（自增方法）的方法
-         * @param {*} context 需要传入一个参数context，可以理解为Vuex实例
-         */
-        actions_increment(context) {
-            context.commit('mutations_increment')
-        }
+    modules: {
+        increment,
+        decrement
     }
 })
 
