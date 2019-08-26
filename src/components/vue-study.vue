659318ad6d032cacfb600d2e6368a39ba5164503
blueju <blueju@foxmail.com>
2019-08-26 13:45:46 +0800

    mapState finish

--- src/components/vue-study.vue
+++ src/components/vue-study.vue
@@ -1,28 +1,44 @@
 <template>
   <div>
-    <!-- <section>{{ this.$store.state.good.good_test_data }}</section> -->
-    <!-- <section>{{ this.$store.getters.get_good_test_data }}</section> -->
-    <div class="increment">
-      <button @click="increment">点击</button>
-      <h1>{{ this.$store.state.number }}</h1>
-    </div>
+    <p>{{ number1 }}</p>
+    <p>{{ number2 }}</p>
+    <p>{{ number3 }}</p>
+    <p>{{ number4 }}</p>
+    <p>{{ number5 }}</p>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   // 给组件命名（可选）
-  name: "HelloWorld",
+  name: "MapState",
   // 数据
   data() {
     return {};
   },
-  // 方法
-  methods: {
-    increment() {
-      // 点击按钮后，我们需要自增加一，我们可以将这个自增加一理解成一个任务，现使用dispatch方法将任务派发给actions_increment
-      this.$store.dispatch("actions_increment");
-    }
+  // 计算属性
+  computed: {
+    number1() {
+      return this.$store.state.number1;
+    },
+    // 简写、快速写法
+    ...mapState({
+      number2: state => state.number2,
+      number3: state => state.number3,
+      number4: state => state.number4,
+      number5: state => state.number5
+    })
+    // 完整写法
+    // ...mapState({
+    //   number4: function(state) {
+    //     return state.number4;
+    //   },
+    //   number5: function(state) {
+    //     return state.number5;
+    //   }
+    // })
   }
 };
 </script>
